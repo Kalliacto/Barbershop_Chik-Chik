85cdca0f2fa96d163ae99dcd0ae54f55745b25bd
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-24 21:12:10 +0300

    Работа с АРI_форма записи

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const API_URL = 'https:chocolate-fuzzy-chanter.glitch.me/api';
+const API_URL = 'https:chocolate-fuzzy-chanter.glitch.me/';
 
 /*
 GET /api - получить список услуг
@@ -142,7 +142,7 @@ const initService = () => {
     addPreload(reserveFieldsetService);
 
     // Получаем данные с сервера
-    fetch(API_URL)
+    fetch(`${API_URL}/api`)
     .then(response => response.json())
     .then(data => {
         // Отрисовываем полученные данные блока СТОИМОСТЬ НАШИХ УСЛУГ
@@ -158,9 +158,149 @@ const initService = () => {
 
 };
 
+const addDisabled = (arr) => {
+    arr.forEach(element => {
+        element.disabled = true;
+        })
+};
+
+const removeDisabled = (arr) => {
+    arr.forEach(element => {
+        element.disabled = false;
+        })
+};
+
+const renderSpec = (wrapper, data) => {
+    const labels = data.map(item => {
+            const label = document.createElement('label');
+            label.classList.add('radio');
+
+            label.innerHTML = `
+                <input class="radio__input" type="radio" name="spec" value="${item.id}">
+                <span class="radio__label radio__label_spec" style="--bg-image: url(${API_URL}${item.img})">${item.name}</span>
+            `;
+            return label;
+    })
+    wrapper.append(...labels);
+};
+
+const renderMonth = (wrapper, data) => {
+    const labels = data.map((item) => {
+        const label = document.createElement('label');
+        label.classList.add('radio');
+
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="month" value="${item}">
+            <span class="radio__label">${
+                new Intl.DateTimeFormat('ru-RU', {
+                    month: 'long'
+                }).format(new Date(item))}</span>
+                `;
+        return label;
+    });
+    wrapper.append(...labels);
+};
+
+const renderDay = (wrapper, data, month) => {
+    const labels = data.map((day) => {
+        const label = document.createElement('label');
+        label.classList.add('radio');
+
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="day" value="${day}">
+            <span class="radio__label">${
+                new Intl.DateTimeFormat('ru-RU', {
+                    month: 'long', day : "numeric"
+                }).format(new Date(`${month}/${day}`))}</span>
+                `;
+        return label;
+    });
+    wrapper.append(...labels);
+};
+
+const renderTime = (wrapper, data) => {
+    const labels = data.map((time) => {
+        const label = document.createElement('label');
+        label.classList.add('radio');
+
+        label.innerHTML = `
+            <input class="radio__input" type="radio" name="time" value="${time}">
+            <span class="radio__label">${time}</span>
+                `;
+        return label;
+    });
+    wrapper.append(...labels);
+};
+
 
 const initReserve = () => {
+    const reserveForm = document.querySelector('.reserve__form');
+    const {fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn,} = reserveForm;
 
+    addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn,]);
+
+    reserveForm.addEventListener('change', async event => {
+        const target = event.target;
+
+        if (target.name === 'service') {
+            addDisabled([fieldspec, fielddata, fieldmonth, fieldday, fieldtime, btn]);
+            fieldspec.innerHTML = '<legend class="reserve__legend">Специалист</legend>';
+            addPreload(fieldspec);
+            // Запрос на сервер
+            const response = await fetch (`${API_URL}/api?service=${target.value}`);
+            // Получение ответа
+            const data = await response.json();
+            
+            renderSpec(fieldspec, data);
+            removePreload(fieldspec);
+            removeDisabled([fieldspec]);
+        }
+
+        if (target.name === 'spec') {
+            addDisabled([fielddata, fieldmonth, fieldday, fieldtime, btn]);
+            addPreload(fieldmonth);
+            
+            const response = await fetch (`${API_URL}/api?spec=${target.value}`);
+            
+            const data = await response.json();
+            fieldmonth.textContent = '';
+            renderMonth(fieldmonth, data);
+            removePreload(fieldmonth);
+            removeDisabled([fielddata, fieldmonth]);
+        }
+
+        if (target.name === 'month') {
+            addDisabled([fieldday, fieldtime, btn]);
+            addPreload(fieldday);
+            
+            const response = await fetch (`${API_URL}/api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}`);
+            
+            const data = await response.json();
+            fieldday.textContent = '';
+            renderDay(fieldday, data, reserveForm.month.value);
+            removePreload(fieldday);
+            removeDisabled([fieldday]);
+        }
+
+        if (target.name === 'day') {
+            addDisabled([fieldtime, btn]);
+            addPreload(fieldtime);
+            
+            const response = await fetch (`${API_URL}/api?spec=${reserveForm.spec.value}&month=${reserveForm.month.value}&day=${target.value}`);
+            
+            const data = await response.json();
+            fieldtime.textContent = '';
+            renderTime(fieldtime, data,);
+            removePreload(fieldtime);
+            removeDisabled([fieldtime]);
+        }
+
+        if (target.name === 'time') {
+            removeDisabled([btn]);
+        }
+
+    // reserveForm.addEventListener('', => {}
+    })
 };
 // --------------------------------------------------------
 const init = () => {
